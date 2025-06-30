b908992c0efb5cd6d2e433bcd63e78fcedda233c
Gaurb <89831065+Gaurb@users.noreply.github.com>
2025-06-30 22:43:34 +0530

    tailwindcss added

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Gaurav This side</h1>
+  )
+}
+
+export default App
--- 04BgChanger/src/App.jsx
+++ 04BgChanger/src/App.jsx
@@ -6,6 +6,10 @@ import './App.css'
 function App() {
   const [color, setColor] = useState("olive")
 
+  let style={
+    weight: "100px"
+  }
+
   return (
     <div
       className="w-full h-screen duration-200"
@@ -16,53 +20,54 @@ function App() {
       >
         <div
           className='fixed flex flex-wrap justify-center shadow-lg gap-3 rounded-xl bg-white px-3 py-1'
+          style={{backgroundColor: color}}
         >
           <button
             onClick={()=>setColor("red")}
-            className=' outline-none rounded-xl px-2 py-1'
-            style={{ background: "red" }}
+            className=' outline-none rounded-xl px-2 py-1 border-2 border-solid'
+            style={{ background: "red", width: "100px" }}
           >
             Red
           </button>
           <button
           onClick={()=>setColor("blue")}
-            className=' outline-none rounded-xl px-2 py-1'
-            style={{ background: "blue" }}
+            className=' outline-none rounded-xl px-2 py-1 border-2 border-solid'
+            style={{ background: "blue" , width: "100px" }}
           >
             blue
           </button>
           <button
           onClick={()=>setColor("green")}
-            className=' outline-none rounded-xl px-2 py-1'
-            style={{ background: "green" }}
+            className=' outline-none rounded-xl px-2 py-1 border-2 border-solid'
+            style={{ background: "green", width: "100px"  }}
           >
             green
           </button>
           <button
           onClick={()=>setColor("yellow")}
-            className=' outline-none rounded-xl px-2 py-1'
-            style={{ background: "yellow" }}
+            className=' outline-none rounded-xl px-2 py-1 border-2 border-solid'
+            style={{ background: "yellow", width: "100px"  }}
           >
             yellow
           </button>
           <button
           onClick={()=>setColor("olive")}
-            className=' outline-none rounded-xl px-2 py-1'
-            style={{ background: "olive" }}
+            className=' outline-none rounded-xl px-2 py-1 border-2 border-solid'
+            style={{ background: "olive", width: "100px"  }}
           >
             olive
           </button>
           <button
           onClick={()=>setColor("skyblue")}
-            className=' outline-none rounded-xl px-2 py-1'
-            style={{ background: "skyblue" }}
+            className=' outline-none rounded-xl px-2 py-1 border-2 border-solid'
+            style={{ background: "skyblue" , width: "100px" }}
           >
             Sky Blue
           </button>
           <button
           onClick={()=>setColor("white")}
-            className=' outline-none rounded-xl px-2 py-1'
-            style={{ background: "white" }}
+            className=' outline-none rounded-xl px-2 py-1 border-2 border-solid'
+            style={{ background: "white", width: "100px"  }}
           >
             white
           </button>
