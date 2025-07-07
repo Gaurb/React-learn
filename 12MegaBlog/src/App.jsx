88587b4e1c9453062a6236811c2710aeaf418a4f
Gaurb <89831065+Gaurb@users.noreply.github.com>
2025-07-07 23:27:13 +0530

    initial commit

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  console.log(import.meta.url.VITE_APPWRITE_URL)
+
+  return (
+    <>
+      <h1>A blog app</h1>
+    </>
+  )
+}
+
+export default App
