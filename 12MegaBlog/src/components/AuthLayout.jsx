8ba4ef4f741febe0bd8979a1a08d9654def0c3cb
Gaurb <89831065+Gaurb@users.noreply.github.com>
2025-07-11 09:08:32 +0530

    initial commit

---
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,20 @@
+import React,{useEffect,useState} from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
+export default function Protected({children, authetication =true}) {
+    const navigate= useNavigate();
+    const[loader,seloader]= useState(true)
+    const authStatus = useSelector(state=> state.auth.status)
+
+    useEffect(()=>{
+        if(authetication && authStatus !== authetication){
+            navigate("/login")
+        }else if(!authetication && authStatus!== authetication){
+            navigate("/")
+        }
+    },[authStatus,navigate,authetication])
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>
+}
